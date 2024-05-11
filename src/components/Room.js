7f221def2f521ba4dd532d45737498656965e581
robertborka <robikaborka.13@gmail.com>
2024-05-11 15:32:48 +0300

    Display each room and apply styling

--- src/components/Room.js
+++ src/components/Room.js
@@ -3,7 +3,7 @@ import React from "react";
 function Room ({room})
 {
     return (
-        <div className='row'>
+        <div className='row box-shadow'>
             <div className="col-md-4">
                 <img src={room.imageurls[0]} className='smallimg'/>
             </div>
@@ -12,6 +12,11 @@ function Room ({room})
                 <p>Max Count : {room.maxcount}</p>
                 <p>Phone Number : {room.phonenumber}</p>
                 <p>Type : {room.type}</p>
+
+                <div style={{float: 'right'}}>
+                    <button className='btn btn-primary'> View Details</button>
+                </div>
+
             </div>
            
         </div>

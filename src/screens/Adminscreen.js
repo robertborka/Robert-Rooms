c368e6ce853261ff91bd51de1e5bb3c2f1518226
robertborka <robikaborka.13@gmail.com>
2024-06-17 03:15:09 +0300

    Add new room Frontend

--- src/screens/Adminscreen.js
+++ src/screens/Adminscreen.js
@@ -4,8 +4,16 @@ import TabPane from "antd/es/tabs/TabPane";
 import axios from "axios";
 import Loader from "../components/Loader";
 import Error from "../components/Error";
+import { max } from "moment";
 
 function Adminscreen() {
+
+    useEffect(() => {
+        if (!JSON.parse(localStorage.getItem("currentUser")).isAdmin) {
+            window.location.href = '/home'
+        }
+
+    }, []);
     return (
         <div className="mt-3 ml-3 mr-3 box-shadow">
             <h6 className="text-center"><b>Admin Panel</b></h6>
@@ -14,13 +22,13 @@ function Adminscreen() {
                     <Bookings />
                 </TabPane>
                 <TabPane tab="Rooms" key="2">
-                    <h1>Rooms</h1>
+                    <Rooms />
                 </TabPane>
                 <TabPane tab="Add Room" key="3">
-                    <h1>Add Room</h1>
+                    <Addroom />
                 </TabPane>
                 <TabPane tab="Users" key="4">
-                    <h1>Users</h1>
+                    <Users />
                 </TabPane>
             </Tabs>
         </div>
@@ -56,12 +64,235 @@ export function Bookings() {
                 <h3>Bookings</h3>
                 {loading && <Loader />}
                 {error && <Error message={error.message} />}
-                {bookings.length ? (
-                    <h3>There are total {bookings.length} bookings</h3>
-                ) : (
-                    !loading && <h3>No bookings available</h3>
-                )}
+                <table className="table table-bordered table-dark">
+                    <thead className="box-shadow">
+                        <tr>
+                            <th>Booking Id</th>
+                            <th>User Id</th>
+                            <th>Room</th>
+                            <th>From</th>
+                            <th>To</th>
+                            <th>Status</th>
+
+
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {bookings.length && (bookings.map(booking => {
+                            return <tr>
+                                <td>{booking._id}</td>
+                                <td>{booking.userid}</td>
+                                <td>{booking.room}</td>
+                                <td>{booking.fromdate}</td>
+                                <td>{booking.todate}</td>
+                                <td>{booking.status}</td>
+
+                            </tr>
+                        }))}
+
+                    </tbody>
+                </table>
+
             </div>
         </div>
     );
+}
+
+export function Rooms() {
+    const [rooms, setrooms] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("/api/rooms/getallrooms");
+                setrooms(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setLoading(false);
+                setError(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="row">
+            <div className="col-md-10">
+                <h3>Rooms</h3>
+                {loading && <Loader />}
+                {error && <Error message={error.message} />}
+                <table className="table table-bordered table-dark">
+                    <thead className="box-shadow">
+                        <tr>
+                            <th>Room Id</th>
+                            <th>Name</th>
+                            <th>Type</th>
+                            <th>Rent per day</th>
+                            <th>Max Count</th>
+                            <th>Phone Number</th>
+
+
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rooms.length && (rooms.map(room => {
+                            return <tr>
+                                <td>{room._id}</td>
+                                <td>{room.name}</td>
+                                <td>{room.type}</td>
+                                <td>{room.rentperday}</td>
+                                <td>{room.maxcount}</td>
+                                <td>{room.phonenumber}</td>
+
+                            </tr>
+                        }))}
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    );
+}
+
+export function Users() {
+    const [users, setusers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("/api/users/getallusers");
+                setusers(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setLoading(false);
+                setError(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="row">
+            <div className="col-md-10">
+                <h3>Users</h3>
+                {loading && <Loader />}
+                <table className="table table-dark table-bordered">
+                    <thead>
+                        <tr>
+                            <th>User Id</th>
+                            <th>User name</th>
+                            <th>User Email</th>
+                            <th>Is Admin</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users && (users.map(user => {
+                            return <tr>
+                                <td>{user._id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.isAdmin ? 'YES' : 'NO'}</td>
+
+                            </tr>
+                        }))}
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    )
+
+}
+
+export function Addroom() {
+
+    const [name, setname] = useState('')
+    const [rentperday, setrentperday] = useState()
+    const [maxcount, setmaxcount] = useState()
+    const [description, setdescription] = useState()
+    const [phonenumber, setphonenumber] = useState()
+    const [type, settype] = useState()
+    const [imageurl1, setimageurl1] = useState()
+    const [imageurl2, setimageurl2] = useState()
+    const [imageurl3, setimageurl3] = useState()
+
+    function addRoom(){
+
+        const newroom={
+            name,
+            rentperday,
+            maxcount,
+            description,
+            phonenumber,
+            type,
+            imageurls:[imageurl1,imageurl2,imageurl3]
+        }
+        
+        console.log(newroom);
+
+    }
+
+
+
+
+
+
+
+    return (
+        <div className="row">
+            <div className="col-md-5">
+
+                <input type="text" className="form-control" placeholder="room name"
+                    value={name} onChange={(e) => { setname(e.target.value) }}
+                />
+                <input type="text" className="form-control" placeholder="rent per day"
+                    value={rentperday} onChange={(e) => { setrentperday(e.target.value) }}
+                />
+                <input type="text" className="form-control" placeholder="max count"
+                    value={maxcount} onChange={(e) => { setmaxcount(e.target.value) }}
+                />
+                <input type="text" className="form-control" placeholder="description"
+                    value={description} onChange={(e) => { setdescription(e.target.value) }}
+                />
+                <input type="text" className="form-control" placeholder="phone number"
+                    value={phonenumber} onChange={(e) => { setphonenumber(e.target.value) }}
+                />
+
+
+            </div>
+
+            <div className="col-md-5">
+                <input type="text" className="form-control" placeholder="type"
+                    value={type} onChange={(e) => { settype(e.target.value) }}
+                />
+                <input type="text" className="form-control" placeholder="Image URL 1"
+                    value={imageurl1} onChange={(e) => { setimageurl1(e.target.value) }}
+                />
+                <input type="text" className="form-control" placeholder="Image URL 2"
+                    value={imageurl2} onChange={(e) => { setimageurl2(e.target.value) }}
+                />
+                <input type="text" className="form-control" placeholder="Image URL 3"
+                    value={imageurl3} onChange={(e) => { setimageurl3(e.target.value) }}
+                />
+
+                <div className="text-right">
+
+                    <button className="btn btn-primary mt-2" onClick={addRoom}>Add Room</button>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
